da9c6fb88281d61c3e991be94ce14d7968165509
chloe-hsieh <chloehsieh@jetfuel.com.tw>
2025-08-21 15:25:45 +0800

    for test vercel

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const serverless = require('serverless-http')
+// const serverless = require('serverless-http')
 const app = require('./server')
 
-module.exports = serverless(app)
+// module.exports = serverless(app)
+module.exports = app
